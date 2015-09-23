4a25c069fc3a89ad1f0011079bd3219a5d9688e0
kennethinsnow <liaoke@gmail.com>
2015-09-23 01:07:32 -0700

    updated to page 52

--- leaderboard.js
+++ leaderboard.js
@@ -1,23 +1,14 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+PlayersList = new Mongo.Collection('players');
+if(Meteor.isServer){
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'otherHelperFunction': function(){
+      return "Some other function"; 
+    }
   });
 }
