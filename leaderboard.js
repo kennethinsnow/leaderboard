8feaac26945a63ee5c8b115caf86b77da0bb6c38
kennethinsnow <liaoke@gmail.com>
2015-09-24 00:51:15 -0700

    updated to page 109

--- leaderboard.js
+++ leaderboard.js
@@ -5,7 +5,8 @@ if(Meteor.isServer){
 if(Meteor.isClient){
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find({}, {sort: {score: -1, name: 1}});
+      var currentUserId = Meteor.userId();
+      return PlayersList.find({createdBy: currentUserId}, {sort: {score: -1, name: 1}});
     },
     'selectedClass': function(){
       var playerId = this._id;
@@ -34,14 +35,19 @@ if(Meteor.isClient){
     },
     'click .remove': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.remove(selectedPlayer);
+      var player = PlayersList.findOne(selectedPlayer).name;
+      var del = confirm("Are you sure you want to remove player " + player);
+      if (del === true){
+        PlayersList.remove(selectedPlayer);
+      }
     }
   });
   Template.addPlayerForm.events({
     'submit form': function(event){
       event.preventDefault();
       var playerNameVar = event.target.playerName.value;
-      PlayersList.insert({name: playerNameVar, score: 0});
+      var currentUserId = Meteor.userId();
+      PlayersList.insert({name: playerNameVar, score: 0, createdBy: currentUserId});
       event.target.playerName.value = "";
     }
   });
